542c7eed8851d2aef526f4587327975368acacd2
andzel0 <andzeloskopje@gmail.com>
2022-12-14 21:07:59 +0000

    cas1

---
+++ README.old.md
@@ -0,0 +1,4 @@
+# repo20
+
+
+## initial
---
+++ src/components/Student.js
@@ -0,0 +1,20 @@
+
+const Student = (props) => {
+
+    if (props.shouldTheComponentShow) {
+        // console.log("true")
+    } else{
+        // console.log("false")
+    }
+
+    props.shouldTheComponentShow ? console.log("true") : console.log("false")
+    // console.log(props)
+    return <div>
+        <h1>Student</h1>
+<div>Name : {props.name}</div>
+<div>Age : {props.age}</div>
+<div>ShouldShow : {props.shouldTheComponentShow}</div>
+    </div>
+}
+
+export default Student
